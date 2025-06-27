bfc2426b3a0f0453c5df9a5cf6475b037ec84be7
Deocomate <deocomate@gmail.com>
2025-06-27 09:13:21 +0700

    update

--- resources/views/client/layouts/layout.blade.php
+++ resources/views/client/layouts/layout.blade.php
@@ -3,34 +3,36 @@
 
 
 <!-- Mirrored from html.themewant.com/moonlit/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 20 Nov 2024 05:08:09 GMT -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="description" content="{{$homestayInfo->description}}">
-    <meta name="keywords" content="{{$homestayInfo->title}}, {{$homestayInfo->description}}">
+    <meta name="description" content="{{ $homestayInfo->description }}">
+    <meta name="keywords" content="{{ $homestayInfo->title }}, {{ $homestayInfo->description }}">
     <meta name="robots" content="index, follow">
     <!-- for open graph social media -->
-    <meta property="og:title" content="{{$homestayInfo->title}}">
-    <meta property="og:description" content="{{$homestayInfo->description}}">
+    <meta property="og:title" content="{{ $homestayInfo->title }}">
+    <meta property="og:description" content="{{ $homestayInfo->description }}">
     <!-- favicon -->
-    <link rel="icon" href="/client/assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="{{ assets('/client/assets/icons/ytyhomestay-icon.ico') }}" type="image/x-icon">
     <!-- title -->
-    <title>@yield("title")</title>
+    <title>@yield('title')</title>
 
-    @include("client.components.styles")
-    @stack("styles")
+    @include('client.components.styles')
+    @stack('styles')
 </head>
 
 <body>
-@include("client.components.header")
+    @include('client.components.header')
 
-@yield("content")
+    @yield('content')
 
-@include("client.components.footer")
-@include("client.components.scripts")
+    @include('client.components.footer')
+    @include('client.components.scripts')
 
-@stack("scripts")
+    @stack('scripts')
 
 </body>
+
 </html>
